c68bbdd7665031072e8ab6a4a322f8d3d2bb85a9
Victor Navarro <navarrov@cardiff.ac.uk>
2024-03-13 15:20:56 +0000

    just pushing to get the app up

--- inst/manual_deployment.R
+++ inst/manual_deployment.R
@@ -1,6 +1,7 @@
 # restart your RStudio/R
 # please remember to install the latest version of calmr from your github page
 devtools::install_github("victor-navarro/calmr.app", force = TRUE)
+devtools::install_github("victor-navarro/calmr", force = TRUE)
 rsconnect::deployApp("C:/Users/sapvn2/calmr.app/inst/calmr_app",
   forceUpdate = TRUE
 )
